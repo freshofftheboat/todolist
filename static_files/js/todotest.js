6cdc5b165ff5cd197497c311dad564f61e4498a5
freshofftheboat <strivelifetime@163.com>
2018-05-03 15:28:52 +0800

    Update todotest.js

--- static_files/js/todotest.js
+++ static_files/js/todotest.js
@@ -127,181 +127,7 @@ class TodoApi {
     }
 }
 
-// var apiGet = (path, callback) => {
-//     var url = 'https://vip.kybmig.cc/sandbox/todo/420704419' + path
-//     ajax('GET', url, '', function(r) {
-//         var t = JSON.parse(r)
-//         callback(t)
-//     })
-// }
-
-// var apiPost = (path, data, callback) => {
-//     var url = 'https://vip.kybmig.cc/sandbox/todo/420704419' + path
-//     data = JSON.stringify(data)
-//     ajax('POST', url, data, function(r) {
-//         var t = JSON.parse(r)
-//         callback(t)
-//     })
-// }
-
-// var apiTodoAll = (callback) => {
-//     // var url = 'https://vip.kybmig.cc/sandbox/todo/420704419/all'
-//     // ajax(method, url, '', function(r) {
-//     //     var todos = JSON.parse(r)
-//     //     callback(todos)
-//     // })
-//
-//     var path = '/all'
-//     apiGet(path, callback)
-// }
-
-// var apiTodoDelete = (todoId, callback) => {
-//     // var method = 'GET'
-//     // var url = 'https://vip.cocode.cc/sandbox/todo/420704419/delete/' + todoId
-//     // ajax(method, url, '', function(r) {
-//     //     var t = json.parse(r)
-//     //     callback(t)
-//     // })
-//
-//     var path = '/delete/' + todoId
-//     apiGet(path, callback)
-// }
-
-// var apiTodoAdd = (data, callback) => {
-//     // var method = 'POST'
-//     // var url = 'https://vip.kybmig.cc/sandbox/todo/420704419/add'
-//     // data = JSON.stringify(data)
-//     // ajax(method, url, data, function(r) {
-//     //     var t = json.parse(r)
-//     //     callback(t)
-//     // })
-//
-//     var path = '/add'
-//     apiPost(path, data, callback)
-// }
-
-// var apiTodoUpdate = (todoId, data, callback) => {
-//     // var method = 'POST'
-//     // var url = 'https://vip.cocode.cc/sandbox/todo/420704419/update/' + todoId
-//     // data = stringify(data)
-//     // ajax(method, url, data, function(r) {
-//     //     var t = json.parse(r)
-//     //     callback(t)
-//     // })
-//
-//     var path = '/update/' + todoId
-//     apiPost(path, data, callback)
-// }
-
 // part3: 绑定事件的函数
-
-// var bindEventAdd = () => {
-//     var container = e('#id-div-todo-container')
-//     container.addEventListener('click', function(event) {
-//         var self = event.target
-//         if (self.classList.contains('todo-add')) {
-//             log('add button click')
-//             // 获取用户输入的内容
-//             var input = e('#id-input-task')
-//             var value = input.value
-//             var data = {
-//                 task: value,
-//             }
-//             // // 用 ajax 发送给服务器
-//             // var method = 'POST'
-//             // var url = 'https://vip.kybmig.cc/sandbox/todo/420704419/add'
-//             // data = JSON.stringify(data)
-//             // ajax(method, url, data, function(r) {
-//             //     var t = JSON.parse(r)
-//             //     log('创建 todo 成功', t)
-//             //     // 向页面插入被创建的 todo
-//             //     insertTodo(t)
-//             // })
-//
-//             apiTodoAdd(data, (todo) => {
-//                 log('创建 todo 成功', todo)
-//                 // 向页面插入被创建的 todo
-//                 insertTodo(todo)
-//             })
-//         }
-//     })
-// }
-
-// var bindEventDelete = () => {
-//     var container = e('#id-div-todo-container')
-//     container.addEventListener('click', function(event) {
-//         var self = event.target
-//         if (self.classList.contains('todo-delete')) {
-//             log('delete button click')
-//             var todoCell = self.closest('.todo-cell')
-//             // 拿到要删除的 todo-id
-//             var todoId = todoCell.dataset.id
-//             // var method = 'GET'
-//             // var url = 'https://vip.cocode.cc/sandbox/todo/420704419/delete/' + todoId
-//             // ajax(method, url, '', function(r) {
-//             //     var t = json.parse(r)
-//             //     log('删除成功'， t)
-//             //     todoCell.remove()
-//             // })
-//
-//             apiTodoDelete(todoId, (todo) => {
-//                 log('删除成功'， todo)
-//                 todoCell.remove()
-//             })
-//         }
-//     })
-// }
-
-// var bindEventEdit = () => {
-//     // 绑定编辑的事件委托
-//     var container = e('#id-div-todo-container')
-//     container.addEventListener('click', function(event) {
-//         var self = event.target
-//         if (self.classList.contains('todo-edit')) {
-//             log('edit button click')
-//             var todoCell = self.closest('.todo-cell')
-//             var task = todoCell.querySelector('.todo-task')
-//             task.contentEditable = true
-//             task.focus()
-//         }
-//     })
-// }
-
-// class ActionApi {
-//     constructor() {
-//         this.api = new TodoApi()
-//     }
-//
-//     Add() {
-//         log('add button click')
-//         // 获取用户输入的内容
-//         var input = e('#id-input-task')
-//         var value = input.value
-//         var data = {
-//             task: value,
-//         }
-//
-//         this.api.update(data, (todo) => {
-//             log('创建 todo 成功', todo)
-//             // 向页面插入被创建的 todo
-//             insertTodo(todo)
-//         })
-//     }
-//
-//     delete(event) {
-//         var self = event.target
-//         log('delete button click')
-//         var todoCell = self.closest('.todo-cell')
-//         // 拿到要删除的 tudo-id
-//         var todoId = todoCell.dataset.id
-//
-//         this.api.delete(todoId, (todo) => {
-//             log('删除成功'， todo)
-//             todoCell.remove()
-//         })
-//     }
-// }
-
 var actionUpdate = (event) => {
     var self = event.target
     log('按了回车')
@@ -312,16 +138,10 @@ var actionUpdate = (event) => {
     // 拿到 todo_id 并且传给删除函数
     // 用 ajax 发送给服务器
     var todoId = todoCell.dataset.id
-    // var method = 'POST'
-    // var url = 'https://vip.cocode.cc/sandbox/todo/420704419/update/' + todoId
     var data = {
         task: self.innerHTML,
     }
-    // data = stringify(data)
-    // ajax(method, url, data, function(r) {
-    //     var t = json.parse(r)
-    //     log('更新成功', t)
-    // })
+
     var api = new TodoApi()
     api.update(todoId, data, (todo) => {
         log('更新成功', todo)
@@ -389,8 +209,6 @@ var actionDone = (event) => {
 
 var bindEventDelegates = () => {
     var container = e('#id-div-todo-container')
-    // // 1. 初始化 Action 实例
-    // var action = new ActionApi()
     // 所有可处理的事件
     // 表驱动法
     var actions = {
@@ -401,14 +219,7 @@ var bindEventDelegates = () => {
     }
     container.addEventListener('click', function(event) {
         var self = event.target
-        // var has = self.classList.contains.bind(self.classList)
-        // if (has('todo-add')) {
-        //     actionAdd()
-        // } else if (has('todo-delete')) {
-        //     actionDelete(event)
-        // } else if (has('todo-edit')) {
-        //     actionEdit(event)
-        // }
+
         var actionName = self.dataset.action
         var action = actions[actionName]
         if (action != undefined) {
@@ -424,26 +235,6 @@ var bindEventUpdate = () => {
         var self = event.target
         if (self.classList.contains('todo-task')) {
             if (event.key == 'Enter') {
-                // log('按了回车')
-                // // 取消事件的默认行为：enter 键的默认换行
-                // event.preventDefault()
-                // self.contentEditable = false
-                // var todoCell = self.closest('.todo-cell')
-                // var todoId = todoCell.dataset.id
-                // // var method = 'POST'
-                // // var url = 'https://vip.cocode.cc/sandbox/todo/420704419/update/' + todoId
-                // var data = {
-                //     task: self.innerHTML
-                // }
-                // // data = stringify(data)
-                // // ajax(method, url, data, function(r) {
-                // //     var t = json.parse(r)
-                // //     log('更新成功', t)
-                // // })
-                //
-                // apiTodoUpdate(todoId, (todo) => {
-                //     log('更新成功', todo)
-                // })
                 actionUpdate(event)
             }
         }
@@ -460,15 +251,6 @@ var bindEvents = () => {
 
 // 2. 载入所有 todo 并插入页面
 var loadTodos = () => {
-    // var method = 'GET'
-    // var url = 'https://vip.kybmig.cc/sandbox/todo/420704419/all'
-    // var data = ''
-    // ajax(method, url, data, function(r) {
-    //     var todos = JSON.parse(r)
-    //     log('载入所有 todos', todos)
-    //     // 向页面插入被创建的 todo
-    //     insertTodos(todos)
-    // })
     var api = new TodoApi()
     api.all((todos) => {
         log('载入所有 todos', todos)
